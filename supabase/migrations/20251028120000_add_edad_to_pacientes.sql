a2d17d9ff96aa78c3102b7326fd1015221e5c7dc
Lalo0502 <eduardo_hernandez13@outlook.es>
2025-10-28 17:52:44 -0500

    Remove supabase migrations from repository

--- supabase/migrations/20251028120000_add_edad_to_pacientes.sql
+++
@@ -1,5 +0,0 @@
--- Agregar columna edad a la tabla pacientes
-ALTER TABLE pacientes
-ADD COLUMN edad INTEGER;
--- Agregar comentario a la columna
-COMMENT ON COLUMN pacientes.edad IS 'Edad del paciente en años';
