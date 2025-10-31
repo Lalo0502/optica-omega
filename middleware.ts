71963f45eaa74d660a3e7b7422382b2bad0a2631
Lalo0502 <eduardo_hernandez13@outlook.es>
2025-10-30 23:17:29 -0500

    chore: Actualización de configuración middleware y next.config

--- middleware.ts
+++ middleware.ts
@@ -54,5 +54,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|svg|jpg|jpeg)$).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
 };
